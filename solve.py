3bd21a89388d8be1ca57e54c298cdc54564eb9e6
Riccardo Invernizzi <nidadoni@gmail.com>
2021-03-11 20:20:51 +0100

    c

--- solve.py
+++ solve.py
@@ -3,16 +3,18 @@ from writer import *
 from random import randint
 
 def main():
-    filenames = ['d']
+    filenames = ['b']
 
     for f in filenames:
         bestScore = 0
         m = Map(f'{f}.in')
-        Old_used_pos = set()
-        cluster = 500
+        final_used = set()
+        cluster = 200
         for k in range(len(m.antenne) // cluster + 1):
+            best_used = set()
             for j in range(10):
-                used_pos = Old_used_pos.copy()
+                tmp_used = set()
+                
                 a = m.width-1
                 b = m.heigth-1
 
@@ -22,19 +24,19 @@ def main():
                     b = (k+1)*cluster
                 for i,ant in enumerate(m.antenne[k*cluster:b]):
                     p = (randint(0,a), randint(0,b))
-                    while p in used_pos:
+                    while p in tmp_used or p in final_used:
                         p = (randint(0,a), randint(0,b))
-                    used_pos.add(p)
+                    tmp_used.add(p)
                     m.setAntennaXY(k*cluster + i, p[0], p[1])
 
                 score = m.tscore()
                 if score > bestScore:
-                    Old_used_pos = used_pos
+                    best_used = tmp_used
                     bestScore = score
                     writesol(f'{f}.out', m)
                     m.save('mappa'+f+'.txt')
                     print('New best score: ', bestScore, ' iterazione numero ', j, 'cluster numero ', k)
-
+            final_used = final_used || best_used
 
 if __name__ == '__main__':
 	main()
